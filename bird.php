d32fa552229a0f7a3d5b78a7f3495a2dc281580b
Xeng Xiong <waterfish_21@yahoo.com>
2018-05-15 21:13:34 -0500

    updated other games with datbase code, score not workint atm

--- bird.php
+++ bird.php
@@ -1,3 +1,8 @@
+<?php
+if (!session_id()) { 
+    session_start();   
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -20,5 +25,34 @@
 			</ul>
 		</nav>
 		</div>
+            <form action="" id="scoreForm" method="post">
+           <input type="submit" name="submitScore" value="Submit Score">
+        </form>
+        <?php
+        require 'dbConnect.php';
+
+        $submitScore = sanitize('submitScore');
+        
+        if (isset($submitScore)) {
+            $gameName = 'Bird vs Bees';
+            $playerScore = 33;
+            $playerName = $_SESSION['userName'];
+
+            try  {
+                 $pdo->beginTransaction();
+                 $query = "INSERT INTO player_scores (playerName, game_name, player_score) VALUES(?, ?, ?)";
+                 $s = $pdo->prepare($query);
+
+                 $s->execute([$playerName, $gameName, $playerScore]);
+                 $pdo->commit();
+
+             } catch (PDOException $ex) {
+                 $pdo->rollback();
+                 throw $ex;
+
+             }    
+        }
+ 
+       ?>
 	</body>
 </html>
--- pong.php
+++ pong.php
@@ -1,7 +1,6 @@
 <?php
 if (!session_id()) { 
-    session_start();
-    
+    session_start();   
 }
 ?>
 <!DOCTYPE html>
